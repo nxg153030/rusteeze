3873e8944ad0c8af4ed53e0a95f0ce88e1e0dc51
Nishant Gurrapadi <nishant.liteteesko@gmail.com>
2025-05-22 23:14:58 -0400

    lexer: add tokenizer functionality

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -9,7 +9,37 @@ impl Lexer {
     }
 
     pub fn next_token(&mut self) -> Option<String> {
-        // Tokenization logic will go here
-        None
+        // Skip whitespace
+        while self.position < self.input.len() && self.input[self.position..].starts_with(char::is_whitespace) {
+            self.position += 1;
+        }
+        // Check if we reached the end of the input
+        if self.position >= self.input.len() {
+            return None;
+        }
+        
+        // Extract the next token
+        let current_char = self.input.chars().nth(self.position).unwrap();
+
+        if current_char.is_digit(10) {
+            // Extract a number
+            let start = self.position;
+            while self.position < self.input.len() && self.input[self.position..].starts_with(char::is_digit) {
+                self.position += 1;
+            }
+            return Some(self.input[start..self.position].to_string());
+        } else if current_char.is_alphabetic() {
+            // Extract an identifier
+            let start = self.position;
+            while self.position < self.input.len() && self.input[self.position..].starts_with(char::is_alphanumeric) {
+                self.position += 1;
+            }
+            return Some(self.input[start..self.position].to_string());
+        } else {
+            // Handle single-character token (e.g., operator or punctuation)
+            self.position += 1;
+            return Some(current_char.to_string());
+        }
+
     }
 }
